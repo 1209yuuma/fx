81eecbdbd0253a0fe2cff391f85feda0adb914a4
yuuma <rijityo.yuuma@icoud.com>
2024-06-17 23:47:11 +0900

    fix

---
+++ db/migrate/20231124112652_add_title_to_diaries.rb
@@ -0,0 +1,5 @@
+class AddAgeToDiaries < ActiveRecord::Migration[7.0]
+  def change
+    add_column :diaries, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_084945) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_27_074008) do
   create_table "comments", force: :cascade do |t|
     t.string "comment_content"
     t.integer "user_id", null: false
@@ -26,7 +26,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_084945) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_diaries_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_favorites_on_post_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -58,5 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_084945) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "diaries", "users"
+  add_foreign_key "favorites", "posts"
+  add_foreign_key "favorites", "users"
   add_foreign_key "posts", "users"
 end
